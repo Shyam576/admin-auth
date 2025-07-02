d073135702db72b0dd1b887f593cd286133ef187
Shyam Basnet <shyambasnet289@gmail.com>
2025-07-02 18:33:44 +0600

    :bug: Optimized page-options dto

--- src/common/dto/page-options.dto.ts
+++ src/common/dto/page-options.dto.ts
@@ -9,22 +9,21 @@ export class PageOptionsDto {
   @EnumFieldOptional(() => Order, {
     default: Order.ASC,
   })
-  readonly order!: Order;
+  readonly order: Order = Order.ASC;
 
   @NumberFieldOptional({
     minimum: 1,
     default: 1,
     int: true,
   })
-  readonly page!: number;
+  readonly page: number = 1;
 
   @NumberFieldOptional({
     minimum: 1,
-    maximum: 50,
     default: 10,
     int: true,
   })
-  readonly take!: number;
+  readonly take: number =10;
 
   get skip(): number {
     return (this.page - 1) * this.take;
